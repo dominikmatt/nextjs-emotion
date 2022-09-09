eeb8d1356c55f1837c08de2689acb578a6c98f76
dominik.matt <mail@matt-dominik.at>
2022-09-09 09:16:16 +0200

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  compiler: {
+    emotion: true, // Change to false to get the icon rendered
+  },
+}
+gith
+export default nextConfig
